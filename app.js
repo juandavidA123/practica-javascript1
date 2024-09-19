2a2085b0dff31e1ff6be5e10e18c6419363a1356
juandavidA123 <alzate2004juan@gmail.com>
2024-09-19 11:15:44 -0500

    pratica bombillo

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 
-document.getElementById("encabezado1").innerHTML = "Panaderia LOS PANDEBONOS";
+/* Esta función carga o ejecuta todo lo que tenga dentro 
+apenas se inicie o abra la página */
+document.addEventListener("DOMContentLoaded", function (){
+
+    //Se modifica el texto del encabezado 1
+    document.getElementById("encabezado1").innerHTML = "Panaderia LOS PANDEBONOS";
+});
